93b56853730ccd8aa01fb5103d89a8d756efd13a
Mirko <mirkosbaglia2001@gmail.com>
2021-04-08 15:30:59 +0200

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -2,11 +2,28 @@ function initVue(){
  new Vue({
    el: '#app',
    data : {
-
-
-
+      todos : [
+       'elem1',
+       'elem2',
+       'elem3',
+       'elem4',
+     ],
+     newTodo : ''
    },
    methods:{
+     clickAggiungi: function(){
+      //console.log('aggiungi' + ' ' + this.newTodo);
+      if(this.newTodo != ''){
+        this.todos.push(this.newTodo)
+        this.newTodo= '';         //
+      }
+      
+     },
+
+     clickX: function(elem){
+       console.log('elimina'+ ' ' +  elem);
+       this.todos.splice(elem, 1); // rimuove l'elemento a cui corrisponde l'indice dato come parametro d'ingresso
+     }
 
 
 
