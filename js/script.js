79da6d118f981f794da856816b22ea52534afe1f
Mirko <mirkosbaglia2001@gmail.com>
2021-04-08 14:12:31 +0200

    Creazione pagine e parte grafica

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function initVue(){
+ new Vue({
+   el: '#app',
+   data : {
+
+
+
+   },
+   methods:{
+
+
+
+   }
+
+  });
+}
+
+function init(){
+  initVue();
+}
+document.addEventListener('DOMContentLoaded', init);
